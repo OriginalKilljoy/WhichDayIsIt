43e157bdf0090eea0b6ee47eb1dea3c50ff35ec8
OriginalKilljoy <162512500+OriginalKilljoy@users.noreply.github.com>
2024-11-23 00:04:03 +0100

    Cleaned up and added some bits and bobs

--- WhichDayIsIt/Program.cs
+++ WhichDayIsIt/Program.cs
@@ -5,46 +5,61 @@
 
 
 
-
+Console.WriteLine("Hello! Choose a number between 1-7 to choose a day:)");
 Run();
 
 void Run()
 {
-    Console.WriteLine("Hello! Choose a number between 1-7 to choose a day:)");
     var userInput = Console.ReadLine();
     int valueOfDay = Convert.ToInt32(userInput);
     switch (valueOfDay)
     {
         case 1:
-            Console.WriteLine("The day is Monday \n Would you like to choose another day? Enter a new number!");
+            Console.WriteLine("The day is Monday!");
+            Thread.Sleep(600);
+            Console.WriteLine("Would you like to choose another day? Enter a new number!");
             Run();
             break;
         case 2:
-            Console.WriteLine("The day is Tuesday\n Would you like to choose another day? Enter a new number!");
+            Console.WriteLine("The day is Tuesday!");
+            Thread.Sleep(600);
+            Console.WriteLine("Would you like to choose another day? Enter a new number!");
             Run();
             break;
         case 3:
-            Console.WriteLine("The day is Wednesday\n Would you like to choose another day? Enter a new number!");
+            Console.WriteLine("The day is Wednesday!");
+            Thread.Sleep(600);
+            Console.WriteLine("Would you like to choose another day? Enter a new number!");
             Run();
             break;
         case 4:
-            Console.WriteLine("The day is Thursday\n Would you like to choose another day? Enter a new number!");
+            Console.WriteLine("The day is Thursday!");
+            Thread.Sleep(600);
+            Console.WriteLine("Would you like to choose another day? Enter a new number!");
             Run();
             break;
         case 5:
-            Console.WriteLine("The day is Friday\n Would you like to choose another day? Enter a new number!");
+            Console.WriteLine("The day is Friday!");
+            Thread.Sleep(600);
+            Console.WriteLine("Would you like to choose another day? Enter a new number!");
             Run();
             break;
         case 6:
-            Console.WriteLine("The day is Saturday\n Would you like to choose another day? Enter a new number!");
+            Console.WriteLine("The day is Saturday!");
+            Thread.Sleep(600);
+            Console.WriteLine("Would you like to choose another day? Enter a new number!");
             Run();
             break;
         case 7:
-            Console.WriteLine("The day is Sunday\n Would you like to choose another day? Enter a new number!");
+            Console.WriteLine("The day is Sunday!");
+            Thread.Sleep(600);
+            Console.WriteLine("Would you like to choose another day? Enter a new number!");
             Run();
             break;
         default:
-            Console.WriteLine("You didn't choose a day");
+            Console.WriteLine("You didn't choose a day, try again please. A number between 1-7:)");
+            Thread.Sleep(600);
+            Run();
             break;
     }
 }
